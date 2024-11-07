b393a92ecc054e6e533dbd56a4a77311ab48c33e
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2024-11-07 11:56:22 +0530

    update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Generative AI Projects",
+    version="0.0.0",
+    author="Puja Kumari",
+    author_email="puja@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
